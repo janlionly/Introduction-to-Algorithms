a48b6c091785fab87af214ab8bed99e436ced3b6
janlionly <jan.lion@me.com>
2020-05-15 19:12:02 +0800

    feat: bubble sort and quick sort implement

--- Sorts/Sorts/Sorts/main.swift
+++ Sorts/Sorts/Sorts/main.swift
@@ -30,7 +30,7 @@ struct Sorts {
     }
     
     private func mergeSort(_ array: inout [Int], leftIndex: Int, rightIndex: Int) {
-        if leftIndex == rightIndex {
+        if leftIndex >= rightIndex {
             return
         }
         let midIndex = (rightIndex + leftIndex)/2
@@ -63,24 +63,106 @@ struct Sorts {
         
 //        print("After merge arr:\(array[leftIndex...rightIndex])\n")
     }
+    
+    // 3. 冒泡排序
+    func bubbleSort(array: inout [Int]) {
+        for i in 0..<array.count - 1 {
+            for j in (i+1..<array.count).reversed() {
+                if array[j - 1] > array[j] {
+                    (array[j - 1], array[j]) = (array[j], array[j - 1])
+                }
+            }
+        }
+    }
+    
+    // 4. 快速排序
+    func quickSort(array: inout [Int]) {
+        quickSort(array: &array, 0, array.count - 1)
+    }
+    
+    private func quickSort(array: inout [Int], _ leftIndex: Int, _ rightIndex: Int) {
+        if leftIndex >= rightIndex {
+            return
+        }
+//        let midIndex = partition(array: &array, leftIndex, rightIndex)
+        let midIndex = randomPartition(array: &array, leftIndex, rightIndex)
+        quickSort(array: &array, leftIndex, midIndex - 1)
+        quickSort(array: &array, midIndex + 1, rightIndex)
+    }
+    
+    private func randomPartition(array: inout [Int], _ leftIndex: Int, _ rightIndex: Int) -> Int {
+        let i = Int.random(in: leftIndex...rightIndex)
+        (array[i], array[rightIndex]) = (array[rightIndex], array[i])
+        return partition(array: &array, leftIndex, rightIndex)
+    }
+    
+    private func partition(array: inout [Int], _ leftIndex: Int, _ rightIndex: Int) -> Int {
+        var lefts: [Int] = []
+        var rights: [Int] = []
+        let key = array[rightIndex]
+        var midIndex = 0
+        
+        //print("Index(left:\(leftIndex),right:\(rightIndex)) Arr:\(array)")
+        //print("Before array:\(array[leftIndex...rightIndex]), key:\(key)")
+        
+        for t in leftIndex...rightIndex-1 {
+            let item = array[t]
+            if item < key {
+                lefts.append(item)
+            } else {
+                rights.append(item)
+            }
+        }
+        
+        var currentIndex = leftIndex
+        
+        for item in lefts {
+            array[currentIndex] = item
+            currentIndex += 1
+        }
+        
+        midIndex = currentIndex
+        array[currentIndex] = key
+        currentIndex += 1
+        
+        for item in rights {
+            array[currentIndex] = item
+            currentIndex += 1
+        }
+
+        //print("After array:\(array[leftIndex...rightIndex]), key:\(key), midIdex:\(midIndex)\n")
+        return midIndex
+    }
 }
 
 
 // Main
-let count = 30000
+let count = 3000
 var A: [Int] = [Int](repeating: 0, count: count)
 
 print("Init array(\(count))")
 for i in 0..<count {
-    A[i] = Int(arc4random() % UInt32(count))
+    A[i] = Int.random(in: 0...count * 100)
 }
 
 print("Starting sorting...")
 let sorter = Sorts()
-let startTime = Date()
+var startTime = Date()
 //sorter.insertionSort(array: &A)
 sorter.mergeSort(array: &A)
-let endTime = Date()
+//sorter.bubbleSort(array: &A)
+//sorter.quickSort(array: &A)
+var endTime = Date()
 
-print("Sort result:\(A), \nProcess seconds:\(endTime.timeIntervalSince(startTime))")
+print("Process seconds:\(endTime.timeIntervalSince(startTime))")
+
+
+//print("Starting sorting...")
+//startTime = Date()
+//sorter.quickSort(array: &A)
+//endTime = Date()
+//print("2Process seconds:\(endTime.timeIntervalSince(startTime))")
+
+
+print("Sort result:\(A)")
 
